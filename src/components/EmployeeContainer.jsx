60e1a44f4a389804dcb2680bdcf19b45f503a45d
Emanuil Vartanyan <mike.vartanyan@yahoo.com>
2020-05-12 19:09:53 -0700

    axios

---
+++ src/components/EmployeeContainer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Employees extends Component {
+    state = {  }
+    render() { 
+        return ( <h3>Hello World</h3> );
+    }
+}
+ 
+export default Employees;
