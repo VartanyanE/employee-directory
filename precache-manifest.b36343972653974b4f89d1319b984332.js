62715d6b7f6a17d9778703dab0218401a14941f1
mikevampm323 <mike.vartanyan@yahoo.com>
2020-05-15 23:08:24 -0700

    Updates

---
+++ precache-manifest.b36343972653974b4f89d1319b984332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93139db32cfdc190bc7722ae2ed6d3c",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "df8d1a90a18d9ac19131",
+    "url": "/employee-directory/static/css/main.b14c05f7.chunk.css"
+  },
+  {
+    "revision": "f6da12a719141fbf9f5a",
+    "url": "/employee-directory/static/js/2.a2b21965.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.a2b21965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8d1a90a18d9ac19131",
+    "url": "/employee-directory/static/js/main.5fc61f12.chunk.js"
+  },
+  {
+    "revision": "a69b56ecf41bf25a56f1",
+    "url": "/employee-directory/static/js/runtime-main.e5aca2d2.js"
+  }
+]);
